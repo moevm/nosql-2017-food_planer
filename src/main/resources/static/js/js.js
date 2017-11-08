ddbca3ac2ba7e68f8de2b56a0f461d2b84cccf65
nikitaseliverstov <nikita77077@inbox.ru>
2017-11-09 01:56:05 +0300

    UI template, for application. Correction of errors and addition of additional fields.

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,76 +1,3 @@
-// var currentTab = 0; // Current tab is set to be the first tab (0)
-// showTab(currentTab); // Display the current tab
-//
-// function showTab(n) {
-//     // This function will display the specified tab of the form ...
-//     var tabs = document.getElementsByClassName("tab");
-//     tabs[n].style.display = "block";
-//     // ... and fix the Previous/Next buttons:
-//     if (n == 0) {
-//         document.getElementById("prevBtn").style.display = "none";
-//     } else {
-//         document.getElementById("prevBtn").style.display = "inline";
-//     }
-//     if (n == (tabs.length - 1)) {
-//         document.getElementById("nextBtn").innerHTML = "Submit";
-//     } else {
-//         document.getElementById("nextBtn").innerHTML = "Next";
-//     }
-//     // ... and run a function that displays the correct step indicator:
-//     fixStepIndicator(n)
-// }
-//
-// function nextPrev(n) {
-//     // This function will figure out which tab to display
-//     var tabs = document.getElementsByClassName("tab");
-//     // Exit the function if any field in the current tab is invalid:
-//     if (n == 1 && !validateForm()) return false;
-//     // Hide the current tab:
-//     tabs[currentTab].style.display = "none";
-//     // Increase or decrease the current tab by 1:
-//     currentTab = currentTab + n;
-//     // if you have reached the end of the form... :
-//     if (currentTab >= tabs.length) {
-//         //...the form gets submitted:
-//         document.getElementById("regForm").submit();
-//         return false;
-//     }
-//     // Otherwise, display the correct tab:
-//     showTab(currentTab);
-// }
-//
-// function validateForm() {
-//     // This function deals with validation of the form fields
-//     var x, y, i, valid = true;
-//     x = document.getElementsByClassName("tab");
-//     y = x[currentTab].getElementsByTagName("input");
-//     // A loop that checks every input field in the current tab:
-//     for (i = 0; i < y.length; i++) {
-//         // If a field is empty...
-//         if (y[i].value == "") {
-//             // add an "invalid" class to the field:
-//             y[i].className += " invalid";
-//             // and set the current valid status to false:
-//             valid = false;
-//         }
-//     }
-//     // If the valid status is true, mark the step as finished and valid:
-//     if (valid) {
-//         document.getElementsByClassName("step")[currentTab].className += " finish";
-//     }
-//     return valid; // return the valid status
-// }
-//
-// function fixStepIndicator(n) {
-//     // This function removes the "active" class of all steps...
-//     var i, x = document.getElementsByClassName("step");
-//     for (i = 0; i < x.length; i++) {
-//         x[i].className = x[i].className.replace(" active", "");
-//     }
-//     //... and adds the "active" class to the current step:
-//     x[n].className += " active";
-// }
-
 $( function() {
     $( "#slider-range" ).slider({
         range: true,
@@ -84,6 +11,22 @@ $( function() {
     $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
         " - " + $( "#slider-range" ).slider( "values", 1 ) );
 } );
+
+$( function() {
+    $( "#slider-rangeForCost" ).slider({
+        range: true,
+        min: 0,
+        max: 50000,
+        values: [ 0, 10000 ],
+        slide: function( event, ui ) {
+            $( "#amountCost" ).val(ui.values[ 0 ] + "₽ - " + ui.values[ 1 ] + "₽" );
+        }
+    });
+    $( "#amountCost" ).val( $( "#slider-rangeForCost" ).slider( "values", 0 ) +
+        "₽ - " + $( "#slider-rangeForCost" ).slider( "values", 1 ) + "₽" );
+} );
+
+
 
 $("body").on('click', '[href*="#"]', function(e){
     var fixed_offset = 100;
